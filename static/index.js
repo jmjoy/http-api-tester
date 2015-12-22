3f141db35daa662c6882361bbca3df86075f4976
jmjoy <918734043@qq.com>
2015-12-22 22:55:38 +0800

    finish index.js bookmark add.

--- static/index.js
+++ static/index.js
@@ -5,7 +5,9 @@ var gPluginKey = "";
 var gResultTest = "";
 
 var configs = {
-    "initDataUrl": "/?act=initData"
+    "initDataUrl":  "/?act=initData",
+    "bookmarkUrl":  "/bookmark",
+    "bookmarksUrl": "/bookmarks"
 };
 
 // TODO Move all global variables to this global object
@@ -17,6 +19,23 @@ var utils = {
     "tplCompile": function(id) {
         var html = $("#" + id).html();
         return Handlebars.compile(html);
+    },
+
+    "ajax": function(url, method, requestData, successCallback, errorCallback) {
+        $.ajax({
+            "url":  url,
+            "type":  method,
+            "data":  JSON.stringify(requestData),
+            "contentType":  "application/json",
+            "cache": false,
+            "dataType": "json",
+            "success":  function(data){
+                successCallback(data);
+            },
+            "error":  function(XMLHttpRequest, textStatus, errorThrown) {
+                errorCallback(textStatus);
+            }
+        });
     }
 };
 
@@ -77,6 +96,12 @@ var page = {
     "message": function (msg) {
         $("#alerter_content").html(msg);
         $("#alerter").modal('show');
+    },
+
+    "inputDialoyMessage": function(msg) {
+        $("#bookmark_add_err").html(msg);
+        $("#bookmark_add_err").removeClass("hidden");
+        return false;
     }
 };
 
@@ -96,12 +121,131 @@ var args = {
     }
 };
 
+var data = {
+    "get": function() {
+        var data = {};
+
+        // 获取数据
+        data.Method = $("#method").bootstrapSwitch("state") ? "GET" : "POST";
+        data.Url = $.trim($("#url").val());
+        data.Bm = {};
+        data.Bm.Switch = $("#bm_switch").bootstrapSwitch("state");
+        data.Bm.N =  parseInt($.trim($("#bm_n").val()));
+        data.Bm.C =  parseInt($.trim($("#bm_c").val()));
+
+        data.Args = [];
+        $("#args_body tr").each(function() {
+            var key = $.trim($(this).find(".arg-key").val());
+            if (key == "") {
+                return false;
+            }
+            data.Args.push({
+                "Key":     key,
+                "Value":   $.trim($(this).find(".arg-value").val()),
+                "Method":  $(this).find(".arg-method").bootstrapSwitch("state") ? "GET" : "POST"
+            });
+        });
+
+        data.Plugin = {};
+        data.Plugin.Data = {};
+        data.Plugin.Key = $("#plugin").val();
+        for (var i in global.plugins[data.Plugin.Key].FieldNames) {
+            data.Plugin.Data[i] = $.trim($("#plugin_"+i).val());
+        }
+
+        // 校验
+        if (isNaN(data.Bm.N) || isNaN(data.Bm.C)) {
+            throw "压测数据必须是数字";
+        }
+        if (data.Bm.N <= 0 || data.Bm.C <= 0) {
+            throw "压测数据必须是正整数";
+        }
+
+        return data;
+    }
+
+};
+
 var plugins = {
     "use": function() {
         var key = $("#plugin").selectpicker('val');
         var plugin = global.plugins[key];
         page.renderPluginPanel(plugin);
     }
+};
+
+var bookmarks = {
+    "handleUse": function() { // TODO
+        var btn = this;
+        $(btn).button('loading');
+
+        var key = $("#bookmark").selectpicker('val');
+        var inputs = {"key": key};
+        $.get(g.bookmarkUrl, inputs, function(data) {
+            $(btn).button('reset');
+
+            if (data.status != 200) {
+                alertMsg(data.msg);
+                return;
+            }
+
+            renderData(data.data);
+
+        }, "json");
+    },
+
+    "add": function() {
+        // validate data
+        try {
+            data.get();
+        } catch (e) {
+            return page.message(e);
+        }
+
+        $("#bookmark_add_err").html("");
+        $("#bookmark_add_err").addClass("hidden");
+        $("#bookmark_add_input").val("");
+        $("#add_dialog").modal("show");
+
+        return true;
+    },
+
+    "handleAdd": function() {
+        var name = $.trim($("#bookmark_add_input").val());
+
+        var bookmark = {};
+        try {
+            bookmark = data.get();
+            bookmark.Name = name;
+        } catch(e) {
+            return page.inputDialoyMessage(e);
+        }
+
+        // 安全禁用
+        var btn = this;
+        $(btn).button('loading');
+
+        utils.ajax(configs.bookmarksUrl, "POST", bookmark, function(data) {
+            $(btn).button('reset'); // 恢复按钮状态
+
+            if (data.Status != 200) {
+                return page.inputDialoyMessage(data.Message);
+            }
+
+            // 成功[
+            var renderData = {"Bookmarks":[bookmark.Name]};
+            var html = templates.bookmarkOptions(renderData);
+            $('#bookmark').append(html);
+            $('#add_dialog').modal('hide');
+            $("#bookmark").selectpicker('val', bookmark.Name);
+            $('#bookmark').selectpicker("refresh");
+
+        }, function(textStatus) {
+            $(btn).button('reset'); // 恢复按钮状态
+            return page.inputDialoyMessage(textStatus);
+        });
+    }
+
 };
 
 // window.onload
@@ -117,6 +261,10 @@ $(function() {
     // set style
     page.refresh();
 
+    // event binding
+    $("#bookmark_add_btn").click(bookmarks.add);
+    $("#bookamrkAddBtn").click(bookmarks.handleAdd);
+
     // 【回调地狱】获取初始化数据
     $.ajax(configs.initDataUrl, {
         "type": "GET",
@@ -128,7 +276,7 @@ $(function() {
                 throw "init error";
             }
 
-            console.log(respData);
+            console.log("initData:", respData);
 
             var data = respData.Data;
             page.renderBookmark(data.Bookmark);
@@ -158,11 +306,10 @@ $(function() {
     // btnclick
     gOptionTpl = returnArgOptionTpl();
     $("#args_add_btn").click(argsAdd);
+
     $("#bookmark_use_btn").click(handleBookmarkUse);
-    $("#bookmark_add_btn").click(bookmarkAdd);
     $("#bookmark_edit_btn").click(bookmarkEdit);
     $("#bookmark_drop_btn").click(bookmarkDrop);
-    $("#bookamrkAddBtn").click(handleBookamrkAdd);
     $("#bookmark_add_input").focus(hiddenErrAlert);
     $("#confirm_dialog_submit_btn").click(clickConfirmDialogSubmitBtn);
     $("#plugin_use_btn").click(pluginUse);
@@ -184,7 +331,7 @@ function pluginUse() {
 }
 
 function handleBookmarkDelete() {
-    var key = $("#bookmark").selectpicker('val');
+    var key = $("#bookmark").selectpicker("val");
     var inputs = {"key": key};
 
     // 安全禁用
@@ -223,25 +370,6 @@ function handleBookmarkDelete() {
     });
 }
 
-function handleBookmarkUse() {
-    var btn = this;
-    $(btn).button('loading');
-
-    var key = $("#bookmark").selectpicker('val');
-    var inputs = {"key": key};
-    $.get(g.bookmarkUrl, inputs, function(data) {
-        $(btn).button('reset');
-
-        if (data.status != 200) {
-            alertMsg(data.msg);
-            return;
-        }
-
-        renderData(data.data);
-
-    }, "json");
-}
-
 function renderData(data) {
     renderBookmarkOptions(data);
     renderPluginOptions(data);
@@ -253,73 +381,6 @@ function renderData(data) {
     var plugin = data.plugins[bookmark.plugin.key];
     renderPluginPanel(plugin);
     renderPlugin(bookmark, plugin);
-}
-
-function handleBookamrkAdd() {
-    var name = $.trim($("#bookmark_add_input").val());
-
-    // 校验
-    var reg = /^\S{1,20}$/;
-    if (!reg.test(name)) {
-        $("#bookmark_add_err").html("书签名称必须为1~20位非空字符");
-        $("#bookmark_add_err").removeClass("hidden");
-        return false;
-    }
-
-    try {
-        var bookmark = getRequestData();
-        bookmark.name = name;
-    } catch(e) {
-        $("#bookmark_add_err").html(e);
-        $("#bookmark_add_err").removeClass("hidden");
-        return false;
-    }
-
-    // 安全禁用
-    var btn = this;
-    $(btn).button('loading');
-
-    // 联网
-    $.ajax({
-        "url":  g.bookmarkUrl,
-        "type":  "POST",
-        "data":  JSON.stringify(bookmark),
-        "contentType":  "application/json",
-        "cache": false,
-        "dataType": "json",
-        "success":  function(data){
-            // 恢复按钮状态
-            $(btn).button('reset');
-
-            if (data.status != 200) {
-                $("#bookmark_add_err").html(data.msg);
-                $("#bookmark_add_err").removeClass("hidden");
-                return false;
-            }
-
-            // 成功
-            var option = {
-                "bookmarks": {}
-            };
-            option.bookmarks[data.data.insertKey] = {"name": data.data.insertName};
-
-            $('#add_dialog').modal('hide');
-            var tpl = $("#bookmark_option_tpl").html();
-            var html = juicer(tpl, option);
-            $('#bookmark').append(html);
-
-            $("#bookmark").selectpicker('val', data.data.insertKey);
-            $('#bookmark').selectpicker("refresh");
-
-        },
-       "error":  function(XMLHttpRequest, textStatus, errorThrown) {
-            // 恢复按钮状态
-            $(btn).button('reset');
-
-            $("#bookmark_add_err").html(textStatus);
-            $("#bookmark_add_err").removeClass("hidden");
-        }
-    });
 }
 
 function handleBookmarkEdit() {
@@ -421,6 +482,7 @@ function bookmarkEdit() {
     $("#confirm_dialog_text").html("您确定要将当前内容替换到选定书签吗？");
     gConfirmDialogSubmitBtnAction = "edit";
     $("#confirm_dialog").modal("show");
+
 }
 
 function bookmarkDrop() {
@@ -442,24 +504,6 @@ function clickConfirmDialogSubmitBtn () {
     }
 }
 
-function bookmarkAdd() {
-    try {
-        getRequestData();
-    } catch(e) {
-        alertMsg(e);
-        return;
-    }
-    $("#bookmark_add_err").html("");
-    $("#bookmark_add_err").addClass("hidden");
-    $("#bookmark_add_input").val("");
-    $("#add_dialog").modal("show");
-}
-
-function alertMsg(msg) {
-    $("#alerter_content").html(msg);
-    $("#alerter").modal('show');
-}
-
 function argsAdd() {
     $("#args_body").append(gOptionTpl);
     $('.switch[type="checkbox"]').bootstrapSwitch();
@@ -476,111 +520,69 @@ function returnArgOptionTpl() {
     return juicer(tpl, empty);
 }
 
-function getRequestData() {
-    var data ={};
-    data.bm = {};
+// function handleSubmit() {
+//     var data = null;
+//     try {
+//         data = getRequestData();
+//     } catch(e) {
+//         alertMsg(e);
+//         return;
+//     }
 
-    // 获取数据
-    data.method = $("#method").bootstrapSwitch("state") ? "GET" : "POST";
-    data.url = $.trim($("#url").val());
-    data.bm.switch = $("#bm_switch").bootstrapSwitch("state");
-    data.bm.n =  parseInt($.trim($("#bm_n").val()));
-    data.bm.c =  parseInt($.trim($("#bm_c").val()));
+//     var btn = this;
+//     $(btn).button('loading');
 
-    data.args = [];
-    $("#args_body tr").each(function() {
-        var key = $.trim($(this).find(".arg-key").val());
-        if (key == "") {
-            return false;
-        }
-        data.args.push({
-            "key":     key,
-            "value":   $.trim($(this).find(".arg-value").val()),
-            "method":  $(this).find(".arg-method").bootstrapSwitch("state") ? "GET" : "POST"
-        });
-    });
+// 	$.ajax({
+//         'url': g.submitUrl,
+// 		'data': JSON.stringify(data),
+// 		'type': 'POST',
+// 		'processData': false,
+// 		'contentType': 'application/json',
+// 		'dataType':	'json',
+// 		'success':	function(data) {
+//             $(btn).button('reset');
 
-    data.plugin = {};
-    data.plugin.data = {};
-    data.plugin.key = gPluginKey;
-    for (var i in gData.plugins[gPluginKey].fields) {
-        data.plugin.data[i] = $.trim($("#plugin_"+i).val());
-    }
+//             if (data.status != 200) {
+//                 alertMsg(data.msg);
+//                 return false;
+//             }
 
-    // 校验
-    if (isNaN(data.bm.n) || isNaN(data.bm.c)) {
-        throw '压测数据必须是数字';
-    }
-    if (data.bm.n <= 0 || data.bm.c <= 0) {
-        throw '压测数据必须是正整数';
-    }
+//             // 成功
+//             renderResult(data.data);
+// 		},
+// 		'error':	function (XMLHttpRequest, textStatus, errorThrown) {
+//             $(btn).button('reset');
+//             alertMsg(e);
+// 		}
+// 	});
 
-    return data;
-}
+//     return false;
+// }
 
-function handleSubmit() {
-    var data = null;
-    try {
-        data = getRequestData();
-    } catch(e) {
-        alertMsg(e);
-        return;
-    }
+// function renderResult(result) {
+//     gResultTest = result.test;
 
-    var btn = this;
-    $(btn).button('loading');
+//     var tpl = $("#result_tpl").html();
+//     var html = juicer(tpl, result);
+//     $("#result_panel").html(html);
 
-	$.ajax({
-        'url': g.submitUrl,
-		'data': JSON.stringify(data),
-		'type': 'POST',
-		'processData': false,
-		'contentType': 'application/json',
-		'dataType':	'json',
-		'success':	function(data) {
-            $(btn).button('reset');
+//     try {
+//         var div = $('<div></div>');
+//         $("#result_test_panel").append(div);
+//         var options = {"dom" : div};
+//         var jf = new JsonFormater(options); //创建对象
+//         jf.doFormat(gResultTest);    //格式化json
 
-            if (data.status != 200) {
-                alertMsg(data.msg);
-                return false;
-            }
+//     } catch(e) {
+//         var iFrame = $('<iframe style="width: 100%; min-height: 350px;"></iframe>');
+//         $("#result_test_panel").append(iFrame);
+//         var iFrameDoc = iFrame[0].contentDocument || iFrame[0].contentWindow.document;
+//         iFrameDoc.write(gResultTest);
+//         iFrameDoc.close();
+//     }
 
-            // 成功
-            renderResult(data.data);
-		},
-		'error':	function (XMLHttpRequest, textStatus, errorThrown) {
-            $(btn).button('reset');
-            alertMsg(e);
-		}
-	});
-
-    return false;
-}
-
-function renderResult(result) {
-    gResultTest = result.test;
-
-    var tpl = $("#result_tpl").html();
-    var html = juicer(tpl, result);
-    $("#result_panel").html(html);
-
-    try {
-        var div = $('<div></div>');
-        $("#result_test_panel").append(div);
-        var options = {"dom" : div};
-        var jf = new JsonFormater(options); //创建对象
-        jf.doFormat(gResultTest);    //格式化json
-
-    } catch(e) {
-        var iFrame = $('<iframe style="width: 100%; min-height: 350px;"></iframe>');
-        $("#result_test_panel").append(iFrame);
-        var iFrameDoc = iFrame[0].contentDocument || iFrame[0].contentWindow.document;
-        iFrameDoc.write(gResultTest);
-        iFrameDoc.close();
-    }
-
-    $("#result_new_tab_btn").click(function() {
-        var newWindowObi=window.open("在新标签中浏览");
-        newWindowObi.document.write(gResultTest);
-    });
-}
+//     $("#result_new_tab_btn").click(function() {
+//         var newWindowObi=window.open("在新标签中浏览");
+//         newWindowObi.document.write(gResultTest);
+//     });
+// }
